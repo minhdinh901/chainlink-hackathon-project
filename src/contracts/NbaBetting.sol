f998d3e5bded5f0aad5205597c73d2fd5974b25a
Minh <m6dinh@ucsd.edu>
2020-09-22 16:37:23 -0700

    added game ID fetching in backend

--- src/contracts/NbaBetting.sol
+++ src/contracts/NbaBetting.sol
@@ -6,22 +6,30 @@ import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
 contract NbaBetting is ChainlinkClient {
     uint8 constant private MAX_GAMES = 3; //Only 3 future games to bet on
     uint8 constant private GAME_OFFSET = 10;
-    uint constant private MAX_DEPOSIT = 999999;
+    uint constant private DATE_FROM_API = 10;
+    uint constant private FUTURE_GAMES = 15;
+    uint8 constant private INT_ASCII_OFFSET = 48;
+    uint constant private MAX_DEPOSIT = 999999000000000000000000;
 
     //Address of Kovan network oracle which contains Get > bytes32 job
     //Found on market.link
     address private ORACLE_ADDRESS = 0xAA1DC356dc4B18f30C347798FD5379F3D77ABC5b;
     //Job ID of Get > bytes32 job
     //Found on market.link
-    string constant private JOB_ID = "b7285d4859da4b289c7861db971baf0a";
-    //LINK payment for Get > bytes32 job (0.1 LINK)
+    bytes32 constant private BYTES_JOB = "b7285d4859da4b289c7861db971baf0a";
+    //Job ID of Get > uint256 job
+    //Found on market.link
+    bytes32 constant private UINT_JOB = "c7dd72ca14b44f0c9b6cfcd4b7ec0a2c";
+    //LINK payment for Get > bytes32 and Get > uint256 jobs (0.1 LINK)
     //1 LINK  = 1 * (10^18)
     uint constant private LINK_PAYMENT = 100000000000000000;
 
     address public owner;
     IERC20 public nbaToken;
-    bytes32 public datetime;
+    string public date;
+    uint public numGames;
 
+    //Array that contains betters and their attributes
     address[] public betters;
     mapping(address => uint) public nbaTokenBalance;
     mapping(address => bool) public hasBetted;
@@ -31,33 +39,121 @@ contract NbaBetting is ChainlinkClient {
     mapping(address => uint) public betAmount2;
     mapping(address => uint) public betAmount3;
 
+    //Array that contains upcoming games and their attributes
+    uint[] public gameIds;
+    mapping(uint => bool) public isStored;
+    mapping(uint => string) public homeTeam;
+    mapping(uint => string) public visitorTeam;
+    mapping(uint => string) public gameDate;
+    mapping(uint => string) public gameTime;
+
     constructor(IERC20 _nbaTokenAddress) public {
+        setPublicChainlinkToken(); //Set the address for the LINK token
         nbaToken = IERC20(_nbaTokenAddress);
         owner = msg.sender;
     }
 
     /* 
-     * Allow server owner to get future game data 
+     * Allow owner to get future games
      * NBA API instructions: https://www.balldontlie.io/#get-all-games
      * World Clock API: http://worldtimeapi.org/ 
      */
-    function getGameData() public /*onlyOwner*/ {
+    function getGameIds() public onlyOwner {
         //Get time data
-        Chainlink.Request memory req1 = 
-        buildChainlinkRequest("b7285d4859da4b289c7861db971baf0a", address(this), 
-                              this.processTimeData.selector);
-        //Get date in PDT
-        req1.add("get", "http://worldtimeapi.org/api/timezone/America/Los_Angeles");
+        Chainlink.Request memory reqDate = 
+            buildChainlinkRequest(BYTES_JOB, address(this), 
+                                  this.processTimeData.selector);
+        //Get date in EDT
+        reqDate.add("get", 
+                 "http://worldtimeapi.org/api/timezone/America/New_York");
         //Path to date and time
-        req1.add("path", "datetime");
-        //Send request to oracle with payment
-        sendChainlinkRequestTo(ORACLE_ADDRESS, req1, LINK_PAYMENT);
+        reqDate.add("path", "datetime");
+        sendChainlinkRequestTo(ORACLE_ADDRESS, reqDate, LINK_PAYMENT);
+
+        //Get all upcoming games
+        Chainlink.Request memory reqGames = 
+            buildChainlinkRequest(UINT_JOB, address(this), 
+                                  this.gamesToGet.selector);
+        //Get next 15 games if possible
+        reqGames.add("get", 
+                 string(abi.encodePacked(
+                 "https://www.balldontlie.io/api/v1/games?start_date=", 
+                 date)));
+        //Path to total number of games
+        reqGames.add("path", "meta.total_count");
+        sendChainlinkRequestTo(ORACLE_ADDRESS, reqGames, LINK_PAYMENT);
+
+        //Get game data for all upcoming games
+        for(uint8 i = 0; i < numGames; i++){
+            bytes memory _gameNum = new bytes(1);
+            _gameNum[0] = byte(INT_ASCII_OFFSET + i);
+            string memory gameNum = string(_gameNum);
+            //Get gameId
+            Chainlink.Request memory reqId = 
+                buildChainlinkRequest(UINT_JOB, address(this), 
+                                      this.storeGameId.selector);
+            reqId.add("get", 
+                      string(abi.encodePacked(
+                      "https://www.balldontlie.io/api/v1/games?start_date=", 
+                      date)));
+            reqId.add("path", string(abi.encodePacked("data.", gameNum, 
+                      ".id")));
+            sendChainlinkRequestTo(ORACLE_ADDRESS, reqId, LINK_PAYMENT);
+        }
     }
 
     function processTimeData(bytes32 _requestId, bytes32 _datetime) public 
              recordChainlinkFulfillment(_requestId) {
-        datetime = _datetime; //TODO: PARSE DATETIME INTO DATE AND TIME
+        //Create arrays to parse date from _datetime
+        bytes memory datetime = abi.encodePacked(_datetime);
+        bytes memory _date = new bytes(DATE_FROM_API);
+
+        //Extract date from datetime
+        for(uint i = 0; i < DATE_FROM_API; i++){
+            _date[i] = datetime[i];
+        }
+
+        //Save date
+        date = string(_date);
     }
+
+    function gamesToGet(bytes32 _requestId, uint _totalCount) public 
+             recordChainlinkFulfillment(_requestId) {
+        if(_totalCount > FUTURE_GAMES){
+            numGames = FUTURE_GAMES;
+        }else{
+            numGames = _totalCount;
+        }
+    }
+
+    function storeGameId(bytes32 _requestId, uint _id) public 
+             recordChainlinkFulfillment(_requestId) {
+        if(!isStored[_id]){
+            gameIds.push(_id);
+        }
+        isStored[_id] = true;
+    }
+
+    /*function getFutureGamesData() public onlyOwner {
+        for(uint i = 0; i < gameIds.length; i++){
+            //Check for invalid game ID
+            if(gameIds[i] == 0){
+                break;
+            }
+
+            //Get home team
+            Chainlink.Request memory reqHome = 
+            buildChainlinkRequest(BYTES_JOB, address(this), 
+                                  this.processTimeData.selector);
+
+            //Get visitor team
+            //Get game time and date
+        }
+    }*/
+
+    /*TODO: function rewardBets() public onlyOwner {
+
+    }*/
 
     /* Allow users to deposit or withdraw NbaTokens */
     function depositOrWithdraw(uint _amount) public {
@@ -107,12 +203,13 @@ contract NbaBetting is ChainlinkClient {
         }
         hasBetted[msg.sender] = true;
     }
-
-    /*function rewardBets() public onlyOwner {
-
-    }
     
-    function getBetInfo() public {
+    /*TODO: function getBetInfo() public {
         
     }*/
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 }
