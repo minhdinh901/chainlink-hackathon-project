ba16c3d865944b10797f79003f5bac847a85a19e
Minh <m6dinh@ucsd.edu>
2020-09-23 02:01:46 -0700

    implemented more API calls and view functions in backend

--- src/contracts/NbaBetting.sol
+++ src/contracts/NbaBetting.sol
@@ -9,10 +9,13 @@ contract NbaBetting is ChainlinkClient {
     uint constant private DATE_FROM_API = 10;
     uint constant private FUTURE_GAMES = 15;
     uint8 constant private INT_ASCII_OFFSET = 48;
+    uint8 constant private MAX_DIGITS = 78;
+    uint8 constant private DECIMAL_BASE = 10;
+    uint8 constant private HALF = 2;
     uint constant private MAX_DEPOSIT = 999999000000000000000000;
 
     //Address of Kovan network oracle which contains Get > bytes32 job
-    //Found on market.link
+    //Alpha Chain - Kovan oracle found on market.link
     address private ORACLE_ADDRESS = 0xAA1DC356dc4B18f30C347798FD5379F3D77ABC5b;
     //Job ID of Get > bytes32 job
     //Found on market.link
@@ -42,9 +45,13 @@ contract NbaBetting is ChainlinkClient {
     //Array that contains upcoming games and their attributes
     uint[] public gameIds;
     mapping(uint => bool) public isStored;
+    string public tempHomeTeam;
     mapping(uint => string) public homeTeam;
+    string public tempVisitorTeam;
     mapping(uint => string) public visitorTeam;
+    string public tempGameDate;
     mapping(uint => string) public gameDate;
+    string public tempGameTime;
     mapping(uint => string) public gameTime;
 
     constructor(IERC20 _nbaTokenAddress) public {
@@ -59,36 +66,33 @@ contract NbaBetting is ChainlinkClient {
      * World Clock API: http://worldtimeapi.org/ 
      */
     function getGameIds() public onlyOwner {
-        //Get time data
+        //Get time data in EDT
         Chainlink.Request memory reqDate = 
             buildChainlinkRequest(BYTES_JOB, address(this), 
                                   this.processTimeData.selector);
-        //Get date in EDT
         reqDate.add("get", 
                  "http://worldtimeapi.org/api/timezone/America/New_York");
-        //Path to date and time
         reqDate.add("path", "datetime");
         sendChainlinkRequestTo(ORACLE_ADDRESS, reqDate, LINK_PAYMENT);
 
-        //Get all upcoming games
+        //Get next 15 games if possible
         Chainlink.Request memory reqGames = 
             buildChainlinkRequest(UINT_JOB, address(this), 
                                   this.gamesToGet.selector);
-        //Get next 15 games if possible
         reqGames.add("get", 
                  string(abi.encodePacked(
                  "https://www.balldontlie.io/api/v1/games?start_date=", 
                  date)));
-        //Path to total number of games
         reqGames.add("path", "meta.total_count");
         sendChainlinkRequestTo(ORACLE_ADDRESS, reqGames, LINK_PAYMENT);
 
-        //Get game data for all upcoming games
+        //Get game IDs for all upcoming games
         for(uint8 i = 0; i < numGames; i++){
+            //Convert i to a string to use in API call
             bytes memory _gameNum = new bytes(1);
             _gameNum[0] = byte(INT_ASCII_OFFSET + i);
             string memory gameNum = string(_gameNum);
-            //Get gameId
+            
             Chainlink.Request memory reqId = 
                 buildChainlinkRequest(UINT_JOB, address(this), 
                                       this.storeGameId.selector);
@@ -102,6 +106,7 @@ contract NbaBetting is ChainlinkClient {
         }
     }
 
+    /* For getGameIds() function only */
     function processTimeData(bytes32 _requestId, bytes32 _datetime) public 
              recordChainlinkFulfillment(_requestId) {
         //Create arrays to parse date from _datetime
@@ -117,6 +122,7 @@ contract NbaBetting is ChainlinkClient {
         date = string(_date);
     }
 
+    /* For getGameIds() function only */
     function gamesToGet(bytes32 _requestId, uint _totalCount) public 
              recordChainlinkFulfillment(_requestId) {
         if(_totalCount > FUTURE_GAMES){
@@ -126,6 +132,7 @@ contract NbaBetting is ChainlinkClient {
         }
     }
 
+    /* For getGameIds() function only */
     function storeGameId(bytes32 _requestId, uint _id) public 
              recordChainlinkFulfillment(_requestId) {
         if(!isStored[_id]){
@@ -134,22 +141,113 @@ contract NbaBetting is ChainlinkClient {
         isStored[_id] = true;
     }
 
-    /*function getFutureGamesData() public onlyOwner {
+    /* Get game data from game IDs stored in gameIds array */
+    function getFutureGamesData() public onlyOwner {
         for(uint i = 0; i < gameIds.length; i++){
             //Check for invalid game ID
             if(gameIds[i] == 0){
                 break;
             }
 
+            //Convert game ID to string
+            uint _gameId = gameIds[i];
+            bytes memory gameId = new bytes(MAX_DIGITS);
+            uint8 leastSignificantDigit = 0;
+            uint8 index = 0;
+            //Store digits in reverse
+            while(_gameId != 0){
+                leastSignificantDigit = uint8(_gameId % DECIMAL_BASE);
+                _gameId = _gameId / DECIMAL_BASE;
+                gameId[index] = byte(INT_ASCII_OFFSET + leastSignificantDigit);
+                index++;
+            }
+            //Reverse digits again to obtain correct string
+            byte temp = "";
+            for(uint8 j = 0; j < index / HALF; j++){
+                temp = gameId[j];
+                gameId[j] = gameId[index - j - 1];
+                gameId[index - j - 1] = temp;
+            }
+            string memory id = string(gameId);
+
             //Get home team
             Chainlink.Request memory reqHome = 
             buildChainlinkRequest(BYTES_JOB, address(this), 
-                                  this.processTimeData.selector);
+                                  this.storeHomeData.selector);
+            reqHome.add("get", string(abi.encodePacked(
+                        "https://www.balldontlie.io/api/v1/games/", id)));
+            reqHome.add("path", "home_team.full_name");
+            sendChainlinkRequestTo(ORACLE_ADDRESS, reqHome, LINK_PAYMENT);
+            homeTeam[gameIds[i]] = tempHomeTeam;
 
             //Get visitor team
-            //Get game time and date
+            Chainlink.Request memory reqVisitor = 
+            buildChainlinkRequest(BYTES_JOB, address(this), 
+                                  this.storeVisitorData.selector);
+            reqVisitor.add("get", string(abi.encodePacked(
+                           "https://www.balldontlie.io/api/v1/games/", id)));
+            reqVisitor.add("path", "visitor_team.full_name");
+            sendChainlinkRequestTo(ORACLE_ADDRESS, reqVisitor, LINK_PAYMENT);
+            visitorTeam[gameIds[i]] = tempVisitorTeam;
+
+            //Get game date
+            Chainlink.Request memory reqDate = 
+            buildChainlinkRequest(BYTES_JOB, address(this), 
+                                  this.storeDateData.selector);
+            reqDate.add("get", string(abi.encodePacked(
+                           "https://www.balldontlie.io/api/v1/games/", id)));
+            reqDate.add("path", "date");
+            sendChainlinkRequestTo(ORACLE_ADDRESS, reqDate, LINK_PAYMENT);
+            gameDate[gameIds[i]] = tempGameDate;
+
+            //Get game time
+            Chainlink.Request memory reqTime = 
+            buildChainlinkRequest(BYTES_JOB, address(this), 
+                                  this.storeTimeData.selector);
+            reqTime.add("get", string(abi.encodePacked(
+                           "https://www.balldontlie.io/api/v1/games/", id)));
+            reqTime.add("path", "status");
+            sendChainlinkRequestTo(ORACLE_ADDRESS, reqTime, LINK_PAYMENT);
+            gameTime[gameIds[i]] = tempGameTime;
         }
-    }*/
+    }
+
+    /* For getFutureGamesData() function only */
+    function storeHomeData(bytes32 _requestId, bytes32 _homeTeam) public 
+             recordChainlinkFulfillment(_requestId) {
+        bytes memory _tempHomeTeam = abi.encodePacked(_homeTeam);
+        tempHomeTeam = string(_tempHomeTeam);
+    }
+
+    /* For getFutureGamesData() function only */
+    function storeVisitorData(bytes32 _requestId, bytes32 _visitorTeam) public 
+             recordChainlinkFulfillment(_requestId) {
+        bytes memory _tempVisitorTeam = abi.encodePacked(_visitorTeam);
+        tempVisitorTeam = string(_tempVisitorTeam);
+    }
+
+    /* For getFutureGamesData() function only */
+    function storeDateData(bytes32 _requestId, bytes32 _date) public 
+             recordChainlinkFulfillment(_requestId) {
+        //Create arrays to parse date from _date
+        bytes memory tempDate = abi.encodePacked(_date);
+        bytes memory transfer = new bytes(DATE_FROM_API);
+
+        //Extract date
+        for(uint i = 0; i < DATE_FROM_API; i++){
+            transfer[i] = tempDate[i];
+        }
+
+        //Save date
+        tempGameDate = string(transfer);
+    }
+
+    /* For getFutureGamesData() function only */
+    function storeTimeData(bytes32 _requestId, bytes32 _time) public 
+             recordChainlinkFulfillment(_requestId) {
+        bytes memory _tempGameTime = abi.encodePacked(_time);
+        tempGameTime = string(_tempGameTime);
+    }
 
     /*TODO: function rewardBets() public onlyOwner {
 
@@ -204,10 +302,27 @@ contract NbaBetting is ChainlinkClient {
         hasBetted[msg.sender] = true;
     }
     
-    /*TODO: function getBetInfo() public {
-        
-    }*/
+    /* Get home team by game ID */
+    function getGameHome(uint _gameId) public view returns (string memory) {
+        return homeTeam[_gameId];
+    }
 
+    /* Get visitor team by game ID */
+    function getGameVisitor(uint _gameId) public view returns (string memory) {
+        return visitorTeam[_gameId];
+    }
+
+    /* Get date by game ID */
+    function getGameDate(uint _gameId) public view returns (string memory) {
+        return gameDate[_gameId];
+    }
+
+    /* Get time by game ID */
+    function getGameTime(uint _gameId) public view returns (string memory) {
+        return gameTime[_gameId];
+    }
+
+    /* Restrict access to owner of contract */
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
